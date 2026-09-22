f779c5353b2fef7665d3d1510d1b397db729ff70
Siddhesh Gite <siddheshgite2004@gmail.com>
2026-09-23 03:31:24 +0530

    added: para

--- apps/customer-portal/src/App.tsx
+++ apps/customer-portal/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <>
     <h1>Siddhesh is the not that much lucky</h1>
     <Button onClick={() => alert(`Amount: ${formatCurrency(1000)}`)}/>
+    <p>Not lucky guy</p>
     </>
 
   );
