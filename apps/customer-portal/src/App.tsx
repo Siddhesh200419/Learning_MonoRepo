67399f84b30b6c873ac3d98a38aad7716092f684
Siddhesh Gite <siddheshgite2004@gmail.com>
2026-09-22 17:54:27 +0530

    added: utils + eslint-config

--- apps/customer-portal/src/App.tsx
+++ apps/customer-portal/src/App.tsx
@@ -1,11 +1,12 @@
 import {Button} from "@repo/ui";
+import {formatCurrency} from "@repo/utils";
 
 function App() {
   
   return (
     <>
     <h1>Siddhesh is the not that much lucky</h1>
-    <Button onClick={() => alert("Button clicked!")}/>
+    <Button onClick={() => alert(`Amount: ${formatCurrency(1000)}`)}/>
     </>
 
   );
